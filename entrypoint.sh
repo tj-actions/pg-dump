a3b4512d8addc208367ef7f77f6344e174c6a681
Tonye Jack <jtonye@ymail.com>
2023-09-07 20:11:01 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,7 +34,16 @@ if [[ -n "$INPUT_POSTGRESQL_VERSION" ]]; then
     echo "Installing postgresql..."
 
     if [[ "$(uname -s)" == "Linux" ]]; then
+        # Create the file repository configuration:
+        sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+        
+        # Import the repository signing key:
+        wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+        
+        # Update the package lists:
         sudo apt-get update
+
+        # Install PostgreSQL
         sudo apt-get install -y "postgresql-$INPUT_POSTGRESQL_VERSION"
     elif [[ "$(uname -s)" == "NT"* ]]; then
         choco install postgresql --version="$INPUT_POSTGRESQL_VERSION" -y
