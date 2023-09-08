68260fe40866eded09e10e604e7438bc3669c3c4
Tonye Jack <jtonye@ymail.com>
2023-09-08 01:26:39 +0000

    Updated the script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+
+echo "::group::pg-dump"
+
+echo "Creating the output directory..."
+
+mkdir -p $(dirname "$INPUT_PATH")
+
+echo "Created the output directory"
+
+echo "Running pg_dump..."
+
+# shellcheck disable=SC2086
+pg_dump $INPUT_OPTIONS -d "$INPUT_DATABASE_URL" > "$INPUT_PATH"
+
+echo "Complete"
+
+echo "::endgroup::"
